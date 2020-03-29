c545b41c38d9911299780755c2b0cd92b820b32a
Hristo <hradoev@abv.bg>
2020-03-29 19:25:08 +0300

    two shirts

--- resources/public/js/threedview.js
+++ resources/public/js/threedview.js
@@ -37,12 +37,12 @@ light.position.set( 0.5, 0.8, 1.8 ).normalize();
 scene.add(light);
 
 //=========== materials =================
-var frontMaterial = new THREE.MeshPhongMaterial({ color: 0xffffff, map: THREE.ImageUtils.loadTexture(imgRender.src),
-  side: THREE.DoubleSide, emissive: 0x001155});
+var frontMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff, map: THREE.ImageUtils.loadTexture(imgRender.src),
+  side: THREE.FrontSide});
 frontMaterial.opacity =1; //opaque color ???
 
-var backMaterial = new THREE.MeshPhongMaterial({ color: 0xffffff,
-  side: THREE.DoubleSide, emissive: 0x001155 });
+var backMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff,
+  side: THREE.FrontSide});
 
 var materials = [frontMaterial, backMaterial];
 var material = new THREE.MeshFaceMaterial(materials);
@@ -115,12 +115,13 @@ for (var i = 0; i < faces.length; i++) {
 
     // Here add custom object
     //drawSphericalSkybox();
-    drawSimpleSkybox();
+    mesh2();
+    //drawSimpleSkybox();
 
 
 
     renderer = new THREE.WebGLRenderer({ alpha: true });
-    renderer.setClearColor(0xaaaaaa, 1);
+    renderer.setClearColor(0xdddddd, 1);
   //  renderer.setClearColor(0x0033ff);
     renderer.setSize(width, height);
 
@@ -237,4 +238,84 @@ camera.aspect = window.innerWidth / window.innerHeight;
 camera.updateProjectionMatrix();
 renderer.setSize(width, height );
 render();
+}
+
+
+//==========================================
+function mesh2(){
+  //=========== materials =================
+  var frontMaterial = new THREE.MeshBasicMaterial({ color: 0xa0a0a0, side: THREE.BackSide, map: THREE.ImageUtils.loadTexture(imgRender.src)});
+  frontMaterial.opacity =1; //opaque color ???
+
+  var backMaterial = new THREE.MeshBasicMaterial({color: 0xa0a0a0, side: THREE.BackSide});
+
+  var materials = [frontMaterial, backMaterial];
+  var material = new THREE.MeshFaceMaterial(materials);
+
+  //============geometry=================
+  var geometry = new THREE.Geometry();
+  var shirtObj = new ShirtObj();
+
+
+  for (var i = 0; i <= shirtObj.v.length - 1; i++)
+      geometry.vertices.push(
+                        new THREE.Vector3(shirtObj.v[i].x, shirtObj.v[i].y, shirtObj.v[i].z));
+
+  var normal = new THREE.Vector3(0, 0, 1);
+  var color = new THREE.Color(0x0000f0);
+
+  for (var i = 0; i <= shirtObj.f_tri.length - 1; i++) {
+
+      geometry.faces.push(new THREE.Face3(shirtObj.f_tri[i].f0, shirtObj.f_tri[i].f2,
+                  shirtObj.f_tri[i].f1, normal));
+
+    }
+
+
+  for (var i = 0; i <= shirtObj.f_quad.length - 1; i++) {
+      geometry.faces.push(new THREE.Face3(shirtObj.f_quad[i].f0, shirtObj.f_quad[i].f1,
+                  shirtObj.f_quad[i].f2, normal, color, 0));
+
+
+      geometry.faces.push(new THREE.Face3(shirtObj.f_quad[i].f0, shirtObj.f_quad[i].f2,
+                  shirtObj.f_quad[i].f3, normal, color, 0));
+
+  }
+
+  geometry.computeBoundingBox();
+
+  var max = geometry.boundingBox.max,
+  min = geometry.boundingBox.min;
+  var offset = new THREE.Vector2(0 - min.x, 0 - min.y);
+  var range = new THREE.Vector2((max.x - min.x), (max.y - min.y));
+  var faces = geometry.faces;
+
+  geometry.faceVertexUvs[0] = [];
+
+  for (var i = 0; i < faces.length; i++) {
+      var v1 = geometry.vertices[faces[i].a];
+      v2 = geometry.vertices[faces[i].b];
+      v3 = geometry.vertices[faces[i].c];
+      var face = geometry.faces[i];
+      if ((v1.z > 0) || (v2.z > 0) || (v3.z > 0))
+        face.materialIndex = 0; // It used first material for now.
+      else face.materialIndex = 1; // It used first material for now.
+
+      geometry.faceVertexUvs[0].push([
+          new THREE.Vector2((v1.x + offset.x) / range.x, (v1.y + offset.y) / range.y),
+          new THREE.Vector2((v2.x + offset.x) / range.x, (v2.y + offset.y) / range.y),
+          new THREE.Vector2((v3.x + offset.x) / range.x, (v3.y + offset.y) / range.y)
+  ])
+
+  }
+      geometry.uvsNeedUpdate = true;
+      var textureMap = THREE.ImageUtils.loadTexture(imgRender.src);
+
+
+    //===========================texture==========================
+
+      mesh = new THREE.Mesh(geometry, material);
+      mesh.position.z = 0;
+      scene.add( mesh );
+
 }
